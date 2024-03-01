8acad2f384b70d6edfbb7047fb5b0c05f6fe1118
Serdna20 <afgarciasa@ufpso.edu.co>
2024-03-01 18:49:27 +0100

    Project Creation

---
+++ oop1/src/main/java/com/mycompany/oop1/Oop1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oop1;
+
+/**
+ *
+ * @author SCISPC28
+ */
+public class Oop1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
