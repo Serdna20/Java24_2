7f585cb64b2eeda799398d8fd522cdc4b577eb69
Serdna20 <afgarciasa@ufpso.edu.co>
2024-03-01 19:38:25 +0100

    Creating a class

--- oop1/src/main/java/com/mycompany/oop1/Oop1.java
+++ oop1/src/main/java/com/mycompany/oop1/Oop1.java
@@ -1,16 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.oop1;
-
-/**
- *
- * @author SCISPC28
- */
 public class Oop1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Character homero = new Character("Homero", "Inspector de seguridad", "A la grande le puse cuca", 33);
+        homero.greeting();
+        Character bart = new Character("Bartolomeo", "Estudiante", "Ay caramba", 10);
+        bart.greeting();
     }
+
+    static class Character {
+        String name;
+        String occupation;
+        String phrase;
+        int age;
+
+        public Character(String name, String occupation, String phrase, int age) {
+            this.name = name;
+            this.occupation = occupation;
+            this.phrase = phrase;
+            this.age = age;
+        }
+
+        void greeting(){
+            System.out.println(this.phrase);
+        }
+    }
+
 }
